a5a9eb55a26bfc26f6413b4f7a67f4e157bbcd68
Oh Sejoon <fpdlrjs55@naver.com>
2025-02-11 17:35:46 +0900

    return값 변경

--- src/main/java/org/scit/project/user/DTO/UserDTO.java
+++ src/main/java/org/scit/project/user/DTO/UserDTO.java
@@ -2,6 +2,8 @@ package org.scit.project.user.DTO;
 
 import java.time.LocalDateTime;
 
+import org.scit.project.user.entity.UserEntity;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -20,11 +22,26 @@ public class UserDTO {
 	
 	private Long userSeq;
 	private String userId;
+	private String userPwd;
 	private String userName;
 	private String userEmail;
 	private LocalDateTime createdAt;
 	private LocalDateTime updatedAt;
 	private boolean isDeleted;
 	
+	public static UserDTO toDTO(UserEntity entity) {
+		
+		return UserDTO.builder()
+				.userSeq(entity.getUserSeq())
+				.userId(entity.getUserId())
+//				.userPwd(entity.get())
+				.userName(entity.getUserName())
+				.userEmail(entity.getUserEmail())
+				.createdAt(entity.getCreatedAt()!= null ? entity.getCreatedAt() : LocalDateTime.now())
+				.updatedAt(entity.getUpdatedAt()!= null ? entity.getCreatedAt() : LocalDateTime.now())
+				.isDeleted(entity.isDeleted())
+				.build();
+				
+	}
 }
 
