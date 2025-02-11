d72ed51e1e3384e5a217895cf58653fa65c9fd75
Oh Sejoon <fpdlrjs55@naver.com>
2025-02-11 18:36:00 +0900

    Email유효성검사 추가

--- src/main/resources/static/js/join.js
+++ src/main/resources/static/js/join.js
@@ -1,8 +1,13 @@
 let idCheck = false;
+let pwdCheck = false;
 $(function () {
     $('#userId').on('blur', confirmId);
     $('#check_all').on('change', toggleAllCheckboxes);
     $('.check-item').on('change', handleCheckItemChange);
+    $('#userPwd').on('focus', clearPwdCheck);
+    $('joinBtn').on('click', join);
+    $('#email').on('blur', validateEmail);
+
 });
 // "전체 동의" 체크 시 모든 체크박스 선택/해제
 function toggleAllCheckboxes() {
@@ -46,5 +51,62 @@ function confirmId() {
             }
         }
     });
-
 }
+// 비밀번호 입력 시 확인란 초기화
+function clearPwdCheck() {
+    $('#userPwdCheck').val('');
+}
+// 회원가입 유효성 검사
+function join() {
+    let userId = $('#userId').val();
+    // 아이디 체크
+    if (userId.trim().length < 3 || userId.trim().length > 12) {
+        alert('아이디는 3~12자 사이로 입력해주세요.');
+        return false;
+    }
+    // 비밀번호 체크
+    let userPwd = $('#userPwd').val();
+    if (userPwd.trim().length < 8 || userPwd.trim().length > 20) {
+        alert('비밀번호는 8~20자 사이로 입력해주세요.');
+        return false;
+    }
+    let userPwdCheck = $('#userPwdCheck').val();
+    if (userPwd !== userPwdCheck) {
+        alert('비밀번호가 일치하지 않습니다.');
+        return false;
+    }
+    $.ajax({
+        url: '/user/join'
+        , method: 'POST'
+        , data: { "userId": userId, "userPwd": userPwd }
+        , success: function (resp) {
+            if (resp) {
+                $('#confirmId').css('color', 'blue');
+                $('#confirmId').html("사용가능한 아이디");
+                idCheck = true;
+            } else {
+                $('#confirmId').css('color', 'red');
+                $('#confirmId').html("사용 불가능한 아이디");
+                idCheck = false;
+            }
+        }
+    });
+}
+// 이메일 유효성 검사
+function validateEmail() {
+    let email = $("#email").val().trim();
+    let emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+    if (email === "") {
+        alert("이메일을 입력해주세요.");
+        return false;
+    }
+    if (!emailPattern.test(email)) {
+        alert("올바른 이메일 형식을 입력해주세요.");
+        return false;
+    }
+}
+
+
+
+
