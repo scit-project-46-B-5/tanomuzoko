5cd33e760c6d0c31faffa9faa33d64a40bd30da1
CHOIHyojun <chlgywns9906@gmail.com>
2025-02-11 14:45:26 +0900

    Recipe 기본 폴더 구성성

---
+++ src/main/java/org/scit/project/recipe/controller/RecipeController.java
@@ -0,0 +1,15 @@
+package org.scit.project.recipe.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class RecipeController {
+
+    // @GetMapping({"/",""})
+    // public String index() {
+
+    // return new String("index");
+    // }
+
+}
---
+++ src/main/java/org/scit/project/recipe/service/RecipeService.java
@@ -0,0 +1,11 @@
+package org.scit.project.recipe.service;
+
+import org.springframework.stereotype.Service;
+
+import jakarta.transaction.Transactional;
+
+@Service
+@Transactional
+public class RecipeService {
+
+}
