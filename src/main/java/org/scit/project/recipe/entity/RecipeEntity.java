c8fe2aeff7fe7c589cc69b6f4e3479a7b8e0bd2e
CHOIHyojun <chlgywns9906@gmail.com>
2025-02-11 15:49:51 +0900

    RecipeEntity 및 RecipeInputKeywordEntity 작성성

--- src/main/java/org/scit/project/recipe/entity/RecipeEntity.java
+++ src/main/java/org/scit/project/recipe/entity/RecipeEntity.java
@@ -1,10 +1,18 @@
 package org.scit.project.recipe.entity;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -18,10 +26,22 @@ import lombok.Setter;
 @NoArgsConstructor
 @Builder
 @Entity
-@Table(name = "sample")
+@Table(name = "recipe")
 public class RecipeEntity {
 
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "sample")
-    private Long resumeNum;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "recipe_seq")
+    private Long recipeSeq;
+
+    @Column(name = "recipe_output_content")
+    private String recipeOutputContent;
+
+    @Column(name = "created_at")
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @OneToMany(mappedBy = "recipeEntity", cascade = CascadeType.REMOVE)
+    @Builder.Default
+    private List<RecipeInputKeywordEntity> recipeInputKeywordEntity = new ArrayList<>();
 }
--- src/main/java/org/scit/project/recipe/entity/RecipeInputKeywordEntity.java
+++ src/main/java/org/scit/project/recipe/entity/RecipeInputKeywordEntity.java
@@ -2,9 +2,12 @@ package org.scit.project.recipe.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -18,11 +21,18 @@ import lombok.Setter;
 @NoArgsConstructor
 @Builder
 @Entity
-@Table(name = "sample")
+@Table(name = "recipe_input_keyword")
 public class RecipeInputKeywordEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "sample")
-    private Long resumeNum;
+    @Column(name = "recipe_seq")
+    private Long recipeSeq;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "recipe_seq", referencedColumnName = "recipe_seq")
+    private RecipeEntity recipeEntity;
+
+    @Column(name = "keyword")
+    private String keyword;
 }
