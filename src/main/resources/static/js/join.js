51708aca7272e1ebe8fa5be8b4449211309181c9
Oh Sejoon <fpdlrjs55@naver.com>
2025-02-11 15:49:29 +0900

    아이디 유효성 검사 및 알림 설정정

--- src/main/resources/static/js/join.js
+++ src/main/resources/static/js/join.js
@@ -1,4 +1,5 @@
 $(function () {
+    $('#userId').on('blur', confirmId);
     $('#check_all').on('change', toggleAllCheckboxes);
     $('.check-item').on('change', handleCheckItemChange);
 });
@@ -11,4 +12,21 @@ function handleCheckItemChange() {
     let total = $('.check-item').length;
     let checked = $('.check-item:checked').length;
     $('#check_all').prop('checked', total === checked);
+}
+function confirmId() {
+    //  아이디 길이체크, 아이디를 적지않았을경우 메세지가 안보이게 설정
+    let userId = $('#userId').val();
+
+    if (userId === "") {
+        $('#confirmId').html("");
+        idCheck = false;
+        return;
+    }
+
+    if (userId.trim().length < 3 || userId.trim().length > 12) {
+        $('#confirmId').css('color', 'red');
+        $('#confirmId').html("아이디는 3~12자리 사이로 입력");
+        return;
+    }
+
 }
