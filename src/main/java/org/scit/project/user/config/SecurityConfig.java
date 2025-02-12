e9f9fc7c5920c777724305b0582af065e19b4a46
Heo Jaewon <112384684+Heo-Jae-Won@users.noreply.github.com>
2025-02-12 16:23:43 +0900

    Feature/login join (#6)
    * 유저 기본틀 생성 작업
    
    * 전체동의 체크박스 설정
    
    * 아이디 유효성 검사 및 알림 설정정
    
    * Security 추가
    
    ---------
    
    Co-authored-by: Oh Sejoon <fpdlrjs55@naver.com>

---
+++ src/main/java/org/scit/project/user/config/SecurityConfig.java
@@ -0,0 +1,64 @@
+package org.scit.project.user.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+	@Bean
+	SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http
+				.authorizeHttpRequests((auth) -> auth
+						.requestMatchers(
+								"/"
+								,"/user/idCheck"
+								,"/user/join"
+								,"/user/joinProc"
+								,"/user/login"
+								, "/images/**"
+								, "/css/**"
+								, "/js/**")
+						.permitAll()
+						.requestMatchers("/admin").hasRole("ADMIN")
+						.requestMatchers("/user/mypage/**").hasAnyRole("ADMIN", "USER")		
+						.anyRequest().authenticated());
+
+		// Custom Login 설정
+		http
+				.formLogin((auth) -> auth
+						.loginPage("/user/login")
+						.loginProcessingUrl("/user/loginProc")
+						.usernameParameter("userId")
+						.passwordParameter("userPwd")
+						.failureUrl("/user/login?error=true")	//loginFailureHandler 가 있으면 이 코드는 없어야한다.
+						.permitAll());
+
+		// logout 설정
+		http
+				.logout((auth) -> auth
+						.logoutUrl("/user/logout")
+						 .logoutSuccessUrl("/")
+						.invalidateHttpSession(true)
+						.clearAuthentication(true));
+
+		// POST 요청시 CSRF 토큰을 요청하므로 (Cross-Site Request Forgery) 비활성화(개발환경)
+		http
+				.csrf((auth) -> auth.disable());
+
+		return http.build();
+	}
+
+	// 단방향 비밀번호 암호화
+	@Bean
+	BCryptPasswordEncoder bCryptPasswordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+}
+
