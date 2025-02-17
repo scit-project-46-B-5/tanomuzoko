e10b8a27b88361a68c88764cb8cd41351fd8472a
Heo Jaewon <112384684+Heo-Jae-Won@users.noreply.github.com>
2025-02-17 16:57:06 +0900

    Feature/login join (#10)
    * 유저 기본틀 생성 작업
    
    * 전체동의 체크박스 설정
    
    * 아이디 유효성 검사 및 알림 설정정
    
    * Security 추가
    
    * return값 변경
    
    * 아이디 중복확인 추가
    
    * Email유효성검사 추가
    
    * 체크박스 유효성 검사 추가
    
    * Email 유효성 검사 변경
    
    * 닉네임 중복검사, 유효성 검사
    
    * roles컬럼추가
    
    * 회원가입 기능 추가
    
    * 로그인 기능 추가
    
    * 회원가입 및 로그인 링크 추가
    
    ---------
    
    Co-authored-by: Oh Sejoon <fpdlrjs55@naver.com>

--- src/main/java/org/scit/project/user/DTO/UserDTO.java
+++ src/main/java/org/scit/project/user/DTO/UserDTO.java
@@ -1,6 +1,8 @@
-package org.scit.project.user.DTO;
+package org.scit.project.user.dto;
 
 import java.time.LocalDateTime;
+
+import org.scit.project.user.entity.UserEntity;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -15,16 +17,33 @@ import lombok.ToString;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-
 public class UserDTO {
 	
 	private Long userSeq;
 	private String userId;
+	private String userPwd;
 	private String userName;
 	private String userEmail;
 	private LocalDateTime createdAt;
 	private LocalDateTime updatedAt;
 	private boolean isDeleted;
+	@Builder.Default
+	private String roles = "ROLE_USER";	
 	
+	public static UserDTO toDTO(UserEntity entity) {
+		
+		return UserDTO.builder()
+				.userSeq(entity.getUserSeq())
+				.userId(entity.getUserId())
+//				.userPwd(entity.get())
+				.userName(entity.getUserName())
+				.userEmail(entity.getUserEmail())
+				.createdAt(entity.getCreatedAt()!= null ? entity.getCreatedAt() : LocalDateTime.now())
+				.updatedAt(entity.getUpdatedAt()!= null ? entity.getCreatedAt() : LocalDateTime.now())
+				.isDeleted(entity.isDeleted())
+				.roles(entity.getRoles())
+				.build();
+				
+	}
 }
 
--- src/main/java/org/scit/project/user/service/UserService.java
+++ src/main/java/org/scit/project/user/service/UserService.java
@@ -1,6 +1,8 @@
 package org.scit.project.user.service;
 
+import org.scit.project.user.dto.UserDTO;
 import org.scit.project.user.repository.UserRepository;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
@@ -11,5 +13,31 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 public class UserService {
 
-	private final UserRepository userRepository; 
+	private final UserRepository userRepository;
+	private final BCryptPasswordEncoder passwprdEncoder;
+
+	public boolean existId(String userId) {
+		boolean result = userRepository.existsByUserId(userId);
+		return !result;
+	}
+
+	public boolean existName(String userName) {
+		boolean result = userRepository.existsByUserName(userName);
+		return !result;
+	}
+
+	//	회원가입 처리
+	public boolean joinProc(UserDTO dto) {
+//		비밀번호 암호화
+	dto.setUserPwd(passwprdEncoder.encode(dto.getUserPwd()));
+		 
+		// DTO를 Entity로 변환하여 DB에 저장
+		org.scit.project.user.entity.UserEntity entity = org.scit.project.user.entity.UserEntity.toEntity(dto);
+		userRepository.save(entity);
+
+		// 저장 후 DB에 아이디가 존재하는지 확인
+		boolean result = userRepository.existsByUserId(dto.getUserId());
+		return result;	// 가입 성공 여부 반환
+	}
+	
 }
