8c7be9e411a199361a870136b539c7e520d6c555
CHOIHyojun <chlgywns9906@gmail.com>
2025-02-17 14:11:46 +0900

    MainPage 최신 게시글 기능 추가

--- src/main/java/org/scit/project/mainpage/controller/MainController.java
+++ src/main/java/org/scit/project/mainpage/controller/MainController.java
@@ -1,15 +1,37 @@
 package org.scit.project.mainpage.controller;
 
+import java.util.List;
+
+import org.scit.project.mainpage.dto.MainDTO;
+import org.scit.project.mainpage.service.MainService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Controller
+@RequiredArgsConstructor
+@Slf4j
 public class MainController {
+
+    private final MainService mainService;
 
     @GetMapping({ "/", "" })
     public String index() {
 
-        return new String("index");
+        return "index";
+    }
+
+    @GetMapping("/posts")
+    @ResponseBody
+    public List<MainDTO> getPosts(@RequestParam(value = "page", defaultValue = "0") int page) {
+
+        List<MainDTO> list = mainService.getPosts(page);
+        
+        return list;
     }
 
 }
--- src/main/java/org/scit/project/mainpage/dto/MainDTO.java
+++ src/main/java/org/scit/project/mainpage/dto/MainDTO.java
@@ -1,5 +1,10 @@
 package org.scit.project.mainpage.dto;
 
+import java.time.LocalDateTime;
+
+import org.scit.project.board.entity.BoardEntity;
+import org.springframework.web.multipart.MultipartFile;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -14,6 +19,31 @@ import lombok.ToString;
 @Setter
 @Getter
 public class MainDTO {
-    private Integer sampleId;
-    private String sampleNmae;
+    private Long boardSeq;
+    private String boardWriter;
+    private String boardTitle;
+    private String boardContent;
+    private Integer hitCount;
+    private LocalDateTime createDate;
+    private LocalDateTime updateDate;
+    private String originalFileName;
+    private String savedFileName;
+    private Boolean isDeleted;
+
+    private MultipartFile uploadFile;
+
+    public static MainDTO toDTO(BoardEntity boardEntity) {
+        return MainDTO.builder()
+                .boardSeq(boardEntity.getBoardSeq())
+                .boardWriter(boardEntity.getBoardWriter())
+                .boardTitle(boardEntity.getBoardTitle())
+                .boardContent(boardEntity.getBoardContent())
+                .hitCount(boardEntity.getHitCount())
+                .createDate(boardEntity.getCreateDate())
+                .updateDate(boardEntity.getUpdateDate())
+                .originalFileName(boardEntity.getOriginalFileName())
+                .savedFileName(boardEntity.getSavedFileName())
+                .isDeleted(boardEntity.getIsDeleted())
+                .build();
+    }
 }
