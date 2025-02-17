ed155ca117c30517e16e43a46e8f6a8ab985c77e
CHOIHyojun <chlgywns9906@gmail.com>
2025-02-17 14:49:01 +0900

    BoardHeart 기본 틀 구성성

---
+++ src/main/java/org/scit/project/board_heart/controller/BoardHeartController.java
@@ -0,0 +1,15 @@
+package org.scit.project.board_heart.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BoardHeartController {
+
+    // @GetMapping({"/",""})
+    // public String index() {
+
+    // return new String("index");
+    // }
+
+}
