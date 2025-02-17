b929a0c8d3154d0ad62083d09f93062172325c0d
CHOIHyojun <chlgywns9906@gmail.com>
2025-02-17 15:20:24 +0900

    BoardHeartEntity 추가가

--- src/main/java/org/scit/project/board_heart/entity/BoardHeartEntity.java
+++ src/main/java/org/scit/project/board_heart/entity/BoardHeartEntity.java
@@ -1,11 +1,19 @@
 package org.scit.project.board_heart.entity;
 
+import org.scit.project.board.entity.BoardEntity;
+import org.scit.project.user.entity.UserEntity;
+
 import jakarta.persistence.Column;
+import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -18,11 +26,23 @@ import lombok.Setter;
 @NoArgsConstructor
 @Builder
 @Entity
-@Table(name = "sample")
+@Table(name = "board_heart", uniqueConstraints = {
+        @UniqueConstraint(columnNames = { "board_seq", "user_seq" })
+})
 public class BoardHeartEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "sample")
-    private Long resumeNum;
+    private Long boardHeartSeq;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "board_seq", insertable = false, updatable = false)
+    private BoardEntity board;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_seq", insertable = false, updatable = false)
+    private UserEntity user;
+
+    @Column(name = "is_hearted", nullable = false)
+    private Boolean isHearted = true;
 }
