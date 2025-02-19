6eb663d4e79de779a5d8e80cb8d281a0694969c5
CHOIHyojun <chlgywns9906@gmail.com>
2025-02-19 17:47:45 +0900

    게시판 공감기능 추가

--- src/main/java/org/scit/project/board_heart/repository/BoardHeartRepository.java
+++ src/main/java/org/scit/project/board_heart/repository/BoardHeartRepository.java
@@ -1,8 +1,14 @@
 package org.scit.project.board_heart.repository;
 
+import java.util.Optional;
+
+import org.scit.project.board.entity.BoardEntity;
 import org.scit.project.board_heart.entity.BoardHeartEntity;
+import org.scit.project.user.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BoardHeartRepository extends JpaRepository<BoardHeartEntity, Long> {
+    Optional<BoardHeartEntity> findByBoardAndUser(BoardEntity board, UserEntity user);
 
+    int countByBoardAndIsHeartedTrue(BoardEntity board); // 공감 개수 조회
 }
