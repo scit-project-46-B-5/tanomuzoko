b0f8e9d7ae2a4292362060740a0d1fc44ca40907
Oh Sejoon <fpdlrjs55@naver.com>
2025-02-20 14:44:34 +0900

    필요없는 파일삭제

--- src/main/java/org/scit/project/user/repository/EmailRepository.java
+++
@@ -1,8 +0,0 @@
-package org.scit.project.user.repository;
-
-import org.scit.project.user.entity.EmailEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmailRepository extends JpaRepository<EmailEntity, Long> {
-
-}
