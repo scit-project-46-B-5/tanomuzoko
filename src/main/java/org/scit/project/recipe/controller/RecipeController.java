6f4725aa713a75865cbba7fe0a6e85a080a4df1e
Heo, JaeWon <ruincraim@gmail.com>
2025-02-21 18:37:35 +0900

    recipe output page로 옮겨가게 변경

--- src/main/java/org/scit/project/recipe/controller/RecipeController.java
+++ src/main/java/org/scit/project/recipe/controller/RecipeController.java
@@ -4,12 +4,18 @@ import org.scit.project.recipe.dto.RecipeUserRequestDTO;
 import org.scit.project.recipe.service.RecipeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Controller
 @RequiredArgsConstructor
+@Slf4j
 public class RecipeController {
 
     private final RecipeService recipeService;
@@ -20,11 +26,18 @@ public class RecipeController {
     // return new String("index");
     // }
 
-    @GetMapping("/recipe/chatGPT")
-    @ResponseBody
-    public String showExample(RecipeUserRequestDTO recipeUserRequestDTO) {
-        return recipeService.sendRequestToChatGPT(recipeUserRequestDTO);
+    @PostMapping("/recipe/chatGPT")
+    public String showExample(@ModelAttribute RecipeUserRequestDTO recipeUserRequestDTO, RedirectAttributes redirectAttributes) {
 
+        recipeService.sendRequestToChatGPT(recipeUserRequestDTO);
+        redirectAttributes.addFlashAttribute("recipe", recipeUserRequestDTO);
+        return "redirect:/recipe/recommend/output";
+
+    }
+
+    @GetMapping("/recipe/recommend/output")
+    public String viewRecipeRecoomendOutput() {
+        return "recipe/recommend_result";
     }
 
     @GetMapping("/recipe/recommend")
