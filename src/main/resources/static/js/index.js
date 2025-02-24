22277f07457e058e484c5a0000cad25c3c021690
CHOIHyojun <chlgywns9906@gmail.com>
2025-02-24 15:39:37 +0900

    슬라이드 함수 중복 코드 제거

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -72,36 +72,32 @@ $(document).ready(function () {
             url: "/top-liked",
             method: "GET",
             success: function (resp) {
-                $('.slider').html(generateSlideHTML(resp));
+                const slider = $('.slider');
 
-                // 슬라이드 초기화
-                if ($('.slider').hasClass('slick-initialized') === false) {
-                    $('.slider').slick({
-                        slidesToShow: 1, // 한 번에 1개의 슬라이드 표시
-                        slidesToScroll: 1, // 한 번에 1개의 슬라이드씩 스크롤
-                        vertical: true, // 수직 슬라이드 활성화
-                        infinite: true, // 무한 반복
-                        arrows: true, // 화살표 네비게이션 활성화
-                        dots: true, // 하단 페이지네이션 활성화
-                        autoplay: true, // 자동 슬라이드
-                        autoplaySpeed: 3000, // 슬라이드 전환 시간 (3초)
-                    });
-                } else {
-                    $('.slider').slick('slickRemove', null, null, true);
-                    $('.slider').slick('unslick');
-                    $('.slider').html(generateSlideHTML(resp));
-                    $('.slider').slick({
-                        slidesToShow: 1,
-                        slidesToScroll: 1,
-                        vertical: true,
-                        infinite: true,
-                        arrows: true,
-                        dots: true,
-                        autoplay: true,
-                        autoplaySpeed: 3000,
-                    });
+                // 기존 슬라이드가 있으면 삭제
+                if (slider.hasClass('slick-initialized')) {
+                    slider.slick('unslick');
                 }
+
+                // 새로운 데이터 적용
+                slider.html(generateSlideHTML(resp));
+
+                // 슬라이드 다시 초기화
+                initializeSlickSlider();
             }
+        });
+    }
+
+    function initializeSlickSlider() {
+        $('.slider').slick({
+            slidesToShow: 1, // 한 번에 1개의 슬라이드 표시
+            slidesToScroll: 1, // 한 번에 1개의 슬라이드씩 스크롤
+            vertical: true, // 수직 슬라이드 활성화
+            infinite: true, // 무한 반복
+            arrows: true, // 화살표 네비게이션 활성화
+            dots: true, // 하단 페이지네이션 활성화
+            autoplay: true, // 자동 슬라이드
+            autoplaySpeed: 3000, // 슬라이드 전환 시간 (3초)
         });
     }
 
