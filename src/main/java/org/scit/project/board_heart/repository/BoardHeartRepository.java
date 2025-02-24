3dcc15f1f7dd158580a7b0d033457e91a0931f3f
Heo Jaewon <112384684+Heo-Jae-Won@users.noreply.github.com>
2025-02-25 08:09:20 +0900

    Feature/main page (#24)
    * MainPage 기본 틀 작성성
    
    * MainRepository 엔티티를 SampleEntity에서 BoardEntity로 변경
    
    * MainPage 최신 게시글 기능 추가
    
    * BoardHeart 기본 틀 구성성
    
    * BoardHeartEntity 추가가
    
    * BoardHeartEntity 추가
    
    * MainPage 최신 게시글 기능 수정
    
    * MainPage의 최신 게시글 a태그에 링크 추가
    
    * 게시글 읽기 기능 추가
    
    * 게시판 공감기능 추가
    
    * BoardHeartCount 불필요한 log.info 제거
    
    * 메인 페이지에서 공감 수 표시 기능 추가
    
    * MainPage 인기 게시글 기능 구현
    
    * index페이지 게시글을 출력하는 중복된 코드 제거
    
    * index페이지 최신 게시글 부분, 마지막 페이지에서 더보기 버튼 숨기기 기능 추가
    
    * index페이지 공감순 탑3 슬라이드 기능 구현
    
    * index 페이지 js 코드 통합하기
    
    * 슬라이드 함수 중복 코드 제거
    
    * 슬라이더에서 하드코딩된 랭킹 게시글 제거
    
    ---------
    
    Co-authored-by: CHOIHyojun <chlgywns9906@gmail.com>

--- src/main/java/org/scit/project/board_heart/repository/BoardHeartRepository.java
+++ src/main/java/org/scit/project/board_heart/repository/BoardHeartRepository.java
@@ -1,14 +1,20 @@
 package org.scit.project.board_heart.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.scit.project.board.entity.BoardEntity;
 import org.scit.project.board_heart.entity.BoardHeartEntity;
 import org.scit.project.user.entity.UserEntity;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface BoardHeartRepository extends JpaRepository<BoardHeartEntity, Long> {
     Optional<BoardHeartEntity> findByBoardAndUser(BoardEntity board, UserEntity user);
 
     int countByBoardAndIsHeartedTrue(BoardEntity board); // 공감 개수 조회
+
+    @Query("SELECT b.boardSeq, COUNT(h) FROM BoardHeartEntity h JOIN h.board b WHERE h.isHearted = true GROUP BY b.boardSeq ORDER BY COUNT(h) DESC")
+    List<Object[]> findTopLikedBoards(Pageable pageable);
 }
