a1e84a5fa1b9544e663deb63e8463ccb7c0ba681
CHOIHyojun <chlgywns9906@gmail.com>
2025-02-25 14:47:18 +0900

    댓글 테이블과 매핑되는 ReplyEntity 생성

--- src/main/java/org/scit/project/reply/entity/ReplyEntity.java
+++ src/main/java/org/scit/project/reply/entity/ReplyEntity.java
@@ -1,11 +1,21 @@
 package org.scit.project.reply.entity;
 
 
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.scit.project.board.entity.BoardEntity;
+import org.scit.project.user.entity.UserEntity;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.PreUpdate;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -19,10 +29,38 @@ import lombok.Setter;
 @NoArgsConstructor
 @Builder
 @Entity
-@Table(name = "sample")
+@Table(name = "reply")
 public class ReplyEntity {
     
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "sample")
-    private Long resumeNum;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "reply_seq")
+    private Long replySeq;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "board_seq", nullable = false)
+    private BoardEntity board;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_seq")
+    private UserEntity user;
+
+    @Column(name = "reply_content", nullable = false)
+    private String replyContent;
+
+    @Column(name = "is_deleted")
+    private Boolean isDeleted;
+
+    @Column(name = "create_date")
+    @CreationTimestamp
+    private LocalDateTime createDate;
+
+    @Column(name = "update_date")
+    private LocalDateTime updateDate;
+
+    @PreUpdate
+    public void onUpdate() {
+        this.updateDate = LocalDateTime.now();
+    }
+
 }
