7eeb018ffdb7faf56a560b006dd91b1459165e66
Heo Jaewon <112384684+Heo-Jae-Won@users.noreply.github.com>
2025-02-27 14:24:04 +0900

    Feature/comment (#27)
    * Comment 기본 폴더 구성
    
    * Comment 기본 폴더명 Reply로 변경
    
    * 댓글 테이블과 매핑되는 ReplyEntity 생성
    
    * 댓글, DB에 등록 기능 구현
    
    * ReplyEntity에서 isDeleted의 default값 추가
    
    * 댓글 출력 기능 구현
    
    * 댓글 삭제 기능 구현
    
    * 댓글 수정 기능 구현현
    
    * 댓글 작성자만 수정, 삭제 제어 기능 구현
    
    * detail.js return문 코드 규격에 맞춰 다시 작성
    
    ---------
    
    Co-authored-by: CHOIHyojun <chlgywns9906@gmail.com>

---
+++ src/main/java/org/scit/project/reply/controller/ReplyController.java
@@ -0,0 +1,56 @@
+package org.scit.project.reply.controller;
+
+import java.util.List;
+
+import org.scit.project.reply.dto.ReplyDTO;
+import org.scit.project.reply.service.ReplyService;
+import org.scit.project.user.dto.LoginUserDetails;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/reply")
+@RequiredArgsConstructor
+public class ReplyController {
+    
+    private final ReplyService replyService;
+
+    // 댓글 등록
+    @PostMapping("/addReply")
+    public void addReply(@ModelAttribute ReplyDTO replyDTO, @AuthenticationPrincipal LoginUserDetails loginUserDetails) {
+        Long userSeq = loginUserDetails.getUserSeq();
+        replyDTO.setUserSeq(userSeq);
+        replyService.addReply(replyDTO);
+    }
+
+    // 댓글 출력
+    @GetMapping("/getReply")
+    public List<ReplyDTO> getReply(@RequestParam(name = "boardSeq") Long boardSeq) {
+        List<ReplyDTO> list = replyService.getReply(boardSeq);
+        return list;
+    }
+
+    // 댓글 삭제
+    @PostMapping("/deleteReply")
+    public void deleteReply(@RequestParam(name = "replySeq") Long replySeq, 
+                            @AuthenticationPrincipal LoginUserDetails loginUserDetails) {
+        Long userSeq = loginUserDetails.getUserSeq();
+        replyService.deleteReply(replySeq, userSeq);
+    }
+
+    // 댓글 수정
+    @PostMapping("/updateReply")
+    public void updateReply(@RequestParam(name = "replySeq") Long replySeq,
+                            @RequestParam("replyContent") String replyContent,
+                            @AuthenticationPrincipal LoginUserDetails loginUserDetails) {
+        Long userSeq = loginUserDetails.getUserSeq();
+        replyService.updateReply(replySeq, replyContent, userSeq);
+    }
+}
