5512557f3315bedbd7157f654608aa992cb8e96b
o_sejoon <fpdlrjs55@naver.com>
2025-03-08 16:47:36 +0900

    Feature/find id pass (#45)
    * header userId가 표시 > userName이 표시되게 변경
    
    * 코드리뷰 내용 반영해서 내용 수정
    
    * 알림창 api를 사용해서 변경
    
    * EmailDTO 필드명 변경으로 인한 수정 작업
    
    * EmailDTO 필드명 변경으로 인한 수정 작업 누락된 부분
    
    * 로그아웃 알림창 변경
    
    * 아이디 찾기 기능 추가
    
    * 비밀번호 찾기 기능추가

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -6,11 +6,23 @@ function logout() {
         url: "/user/logout", // Spring Security 설정된 로그아웃 URL
         type: "POST",
         success: function () {
-            alert("로그아웃 되었습니다.");
+            Swal.fire({
+                icon: 'success',
+                title: '성공!',
+                text: '로그아웃이 완료되었습니다다.',
+                confirmButtonColor: '#ff7f50',
+                confirmButtonText: '확인',
+            });
             window.location.href = "/"; // ✅ 로그아웃 후 메인 페이지로 이동
         },
         error: function () {
-            alert("로그아웃 실패! 다시 시도해주세요.");
+            Swal.fire({
+                icon: 'warning',
+                title: '실패',
+                text: '로그아웃 실패! 다시 시도해주세요.',
+                confirmButtonColor: '#ff7f50',
+                confirmButtonText: '확인',
+            });
         }
     });
 };
