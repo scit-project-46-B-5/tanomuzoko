fa919b01ed80ccdc51dbf06a5b514f15f4cad5fd
CHOIHyojun <chlgywns9906@gmail.com>
2025-03-10 16:21:32 +0900

    답글 기능 구현 및 주석 추가

--- src/main/java/org/scit/project/reply/repository/ReplyRepository.java
+++ src/main/java/org/scit/project/reply/repository/ReplyRepository.java
@@ -1,13 +1,17 @@
 package org.scit.project.reply.repository;
 
+import java.util.List;
+
 import org.scit.project.board.entity.BoardEntity;
 import org.scit.project.reply.entity.ReplyEntity;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ReplyRepository extends JpaRepository<ReplyEntity, Long>{
 
-    Page<ReplyEntity> findAllByBoardAndIsDeletedFalse(BoardEntity board, Pageable pageable);
-    
+    List<ReplyEntity> findByParentReplyAndIsDeletedFalse(ReplyEntity parentReply);
+
+    List<ReplyEntity> findByBoardAndParentReplyIsNullAndIsDeletedFalse(BoardEntity board, Sort sort);
 }
