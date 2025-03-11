612c0ac62d68c694206102802079ef640303f352
Ming <denebolx@gmail.com>
2025-03-11 17:07:44 +0900

    답글 버튼 추가 및 디자인 수정정

--- src/main/resources/static/js/detail.js
+++ src/main/resources/static/js/detail.js
@@ -114,9 +114,12 @@ function renderComment(item, loginId, isChild) {
                 ${hasMore ? '<button class="more-btn" onclick="toggleExpand(this)">더보기</button>' : ''}
             </div>
     `;
-
+    if (!isChild && loginId) {
+        tag += `<button class="reply-btn" onclick="showReplyForm(${item.replySeq})">답글</button>`;
+    }
     tag += `</div>`;
     tag += `<div id="reply-form-${item.replySeq}" class="reply-form" style="display: none; margin-left: 30px;"></div>`;
+    // 로그인한 사용자만 답글 버튼 보이게 설정
 
     return tag;
 }
