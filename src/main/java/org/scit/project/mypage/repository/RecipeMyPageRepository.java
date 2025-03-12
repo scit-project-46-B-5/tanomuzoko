0c16ffacd7a7a0cd5eb173d8b61383bcb8a5828a
Heo-Jae-Won <ckdwhgood2@naver.com>
2025-03-12 23:53:17 +0900

    불필요한 쿼리 대신 한번에 가져오게 수정

--- src/main/java/org/scit/project/mypage/repository/RecipeMyPageRepository.java
+++ src/main/java/org/scit/project/mypage/repository/RecipeMyPageRepository.java
@@ -3,6 +3,7 @@ package org.scit.project.mypage.repository;
 import org.scit.project.recipe.entity.RecipeEntity;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -10,11 +11,13 @@ import org.springframework.data.repository.query.Param;
 
 public interface RecipeMyPageRepository extends JpaRepository<RecipeEntity, Long>{
     
+    @EntityGraph(attributePaths = {"recipeOutputEntity", "recipeInputKeywordEntityList"})
     @Query("""
         SELECT DISTINCT r FROM RecipeEntity r
-        JOIN r.recipeInputKeywordEntityList k
-        WHERE r.userEntity.userSeq = :userSeq
-        AND r.recipeOutputEntity IS NOT NULL
+        INNER JOIN r.recipeInputKeywordEntityList k
+        INNER JOIN r.recipeOutputEntity j
+        INNER JOIN r.userEntity u
+        WHERE u.userSeq = :userSeq
     """)
     Page<RecipeEntity> findRecipesWithPagination(
         @Param("userSeq") Long userSeq,
