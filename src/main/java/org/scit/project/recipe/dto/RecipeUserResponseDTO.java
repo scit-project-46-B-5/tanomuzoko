a75122dc6984c45d959aeb770501357cc95a7c1c
Heo Jaewon <112384684+Heo-Jae-Won@users.noreply.github.com>
2025-03-12 19:09:56 +0900

    Feature/recipe recommend (#59)
    * Recipe 기본 폴더 구성성
    
    * add fk condition on recipe_input_keyword table
    
    * fixed JPA annotation PK
    
    이유: JPA에서는 식별 관계에서 PK이자 FK인 컬럼은 single PK column으론
    구현이 불가능하다. 그러한 이유로 DDL을 수정하여 진행한다.
    
    방법: PK를 복합키로 구성하는 형태로 Entity를 만들고, @MapsID를 활용한다.
    
    * RecipeEntity 및 RecipeInputKeywordEntity 작성성
    
    * recipe-keyword table 식별 관계 구성을 위한 entity 수정
    
    * recipe 관련 기본DTO 추가가
    
    * Open ai 서버 연동
    
    * js 경로 변경경
    
    * open api 관련 사항항 추가
    
    향후 암호화 예정
    
    * 레시피 추천 페이지 경로 mapping 추가
    
    * recipe_output_content ERD 변경
    
    recipe_title column 추가가
    
    * form submit 방식 작동하게 변경
    
    1. form action url 추가
    2. button type submit으로 변경
    
    * 소비자가 선택한 div와 radio 값을 연동하게 변경
    
    * 파일 경로 절대 경로로 변경경
    
    * heaader fragment를 이용하게 방식 변경
    
    * 추상적인 field명명을 실제 name에 맞게 변경
    
    * meal에서 음식용도를 알 수 있게 usage로 field, name명 변경
    
    * header 역할 fragment에 header class 추가가
    
    * recipe 실제 input을 받아오게 변경
    
    * board_content column tag 전부 넣게 변경하여 longtext로 변경
    saved_file_name base64값을 저장하게 변경되어 longtext로 변경
    
    * recipe output page로 옮겨가게 변경
    
    * recipe prompt 양식 변경
    
    * added recipe output fetching to html
    
    * @ModelAttritubte constructor 문제로 protected -> public 으로 constructor 변경
    
    * recipeSave_mypage.html과의 호환을 위해 class 명 추가
    
    * recipe 선택 조건은 별도의 DTO로 분리
    
    * refresh에 취약하지 않게 session으로 recipe 관리하게 변경 및 json구조로 변경
    
    * recipe history를 남기기 위한 backend 개발 added
    
    * recipe history 저장 기능 추가
    
    1. history는 1번 저장하면 다시 저장하지 못하게 구현
    2. 저장이 실패할 경우에는 다시 retry가 가능하게 구현
    
    * css 및 url 적용용
    
    * mypage에서 recipe 정보가 보이게 변경
    
    * MyRecipePageController 별도로 만듦에 따라 삭제제
    
    * 과정 이해하기 쉽게 private method로 따로 뺌뺌
    
    * session 시간 초과됐을 때 처리 추가가
    
    * tab 2칸 짜리 4칸 짜리로 변경경
    
    * objectMapper bean customize 하지 않으므로 설정에서 제거거
    
    * import organize
    
    * mypage recipe pagination 적용
    
    * recipe mypage service method명 변경 및 organize import
    
    ---------
    
    Co-authored-by: CHOIHyojun <chlgywns9906@gmail.com>

--- src/main/java/org/scit/project/recipe/dto/RecipeUserResponseDTO.java
+++ src/main/java/org/scit/project/recipe/dto/RecipeUserResponseDTO.java
@@ -1,8 +1,5 @@
 package org.scit.project.recipe.dto;
 
-import java.util.ArrayList;
-
-import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -28,6 +25,15 @@ public class RecipeUserResponseDTO  {
                                     .ingredients(ingredients)
                                     .cookingMethods(cookingMethods)
                                     .recipeConditionDTO(recipeConditionDTO)
+                                    .build();
+    }
+
+    public static RecipeUserResponseDTO empty() {
+        return RecipeUserResponseDTO.builder()
+                                    .title("session이 만료되었습니다.")
+                                    .ingredients(new String[]{"session이 만료되었습니다."})
+                                    .cookingMethods(new String[]{"session이 만료되었습니다."})
+                                    .recipeConditionDTO(RecipeConditionDTO.empty())
                                     .build();
     }
 }
