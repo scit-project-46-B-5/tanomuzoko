9f5c8dad2371b56240e27dc6fb6b2e7ff87eece0
Heo-Jae-Won <ckdwhgood2@naver.com>
2025-03-13 23:34:30 +0900

    N+1 해결을 위해 query 처리 방식 변경

--- src/main/java/org/scit/project/reply/repository/ReplyRepository.java
+++ src/main/java/org/scit/project/reply/repository/ReplyRepository.java
@@ -4,12 +4,26 @@ import java.util.List;
 
 import org.scit.project.board.entity.BoardEntity;
 import org.scit.project.reply.entity.ReplyEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ReplyRepository extends JpaRepository<ReplyEntity, Long>{
 
     List<ReplyEntity> findByParentReplyAndIsDeletedFalse(ReplyEntity parentReply);
 
     List<ReplyEntity> findByBoardAndParentReplyIsNullAndIsDeletedFalse(BoardEntity board, Sort sort);
+
+    @Query("""
+        SELECT DISTINCT r FROM ReplyEntity r 
+        LEFT JOIN FETCH r.childReplies c
+        WHERE r.board = :board 
+        AND (r.parentReply IS NULL OR r.parentReply IN 
+        (SELECT p FROM ReplyEntity p WHERE p.board = :board AND p.parentReply IS NULL))
+        ORDER BY COALESCE(r.parentReply.replySeq, r.replySeq)
+    """)
+    Page<ReplyEntity> findRepliesByBoard(@Param("board") BoardEntity board, Pageable pageable);
 }
