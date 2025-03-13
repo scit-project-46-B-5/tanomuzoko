61a616eeb5de4f928db4da85e511fd49d9ed9390
Heo Jaewon <112384684+Heo-Jae-Won@users.noreply.github.com>
2025-03-13 21:55:00 +0900

    Feature/find id pass (#69)
    * header userId가 표시 > userName이 표시되게 변경
    
    * 코드리뷰 내용 반영해서 내용 수정
    
    * 알림창 api를 사용해서 변경
    
    * EmailDTO 필드명 변경으로 인한 수정 작업
    
    * EmailDTO 필드명 변경으로 인한 수정 작업 누락된 부분
    
    * 로그아웃 알림창 변경
    
    * 아이디 찾기 기능 추가
    
    * 비밀번호 찾기 기능추가
    
    * 회원가입시 탈퇴된 회원정보도 중복체크하는것 방지
    
    * SENDEREMAIL를 application.properties에서 value값으로 가져오게 변경
    
    * EmailService  senderEmail을 외부참조로 변경으로 인해 value값으로 불러오게 변경
    
    * 중복확인 기능 로직 변경으로 인한 수정
    
    * header 타임리프 설정 미스로 인한 수정
    
    * 로그아웃 알림창 변경 및 html에 js인식
    
    * email인증 알림창 > 메일송신 순서로 변경
    
    * 회원가입 페이지 닉네임 입력순서 변경
    
    * 탈퇴한 회원 계정 복원기능 추가
    
    * 비밀번호 찾기를 할때 가입된 값과 다른 값을 넣을경우 로그인페이지로 리다이렉트 안되게 js 수정
    
    * DTO 유효성 검사 추가
    
    * api마다 요구하는 DTO 세분화하여 @Valid 적용
    
    ---------
    
    Co-authored-by: Oh Sejoon <fpdlrjs55@naver.com>

---
+++ src/main/java/org/scit/project/user/dto/EmailCheckDTO.java
@@ -0,0 +1,13 @@
+package org.scit.project.user.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class EmailCheckDTO {
+    // ✅ 이메일 주소 (Entity의 필드명과 통일)
+	@NotBlank(message = "이메일은 필수입니다.")
+	@Email(message = "올바른 이메일 형식이어야 합니다.")
+    private String userEmail;
+}
