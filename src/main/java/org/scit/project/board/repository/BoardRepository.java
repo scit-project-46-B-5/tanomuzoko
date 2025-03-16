d4564c7394fb6ba6d1d114e4a68eded7652964a2
Heo-Jae-Won <ckdwhgood2@naver.com>
2025-03-16 10:53:54 +0900

    안쓰게 된 natvie query 삭제

--- src/main/java/org/scit/project/board/repository/BoardRepository.java
+++ src/main/java/org/scit/project/board/repository/BoardRepository.java
@@ -18,13 +18,4 @@ public interface BoardRepository extends JpaRepository<BoardEntity, Long> {
     @Modifying
     @Query("update BoardEntity b set b.hitCount = b.hitCount + 1 where b.boardSeq = :boardSeq")
     void incrementHitCount(@Param("boardSeq") Long boardSeq);
-    
-    // 현재 로그인한 사용자의 레시피 목록 조회 (레시피 id와 제목)
-    // recipe와 recipe_output_content 테이블을 조인하고, board 테이블에 해당 recipe가 등록되지 않은 레시피만 조회
-    @Query(value = "SELECT r.recipe_seq, roc.recipe_title FROM recipe r " +
-                   "JOIN recipe_output_content roc ON r.recipe_seq = roc.recipe_seq " +
-                   "WHERE r.user_seq = :userSeq " +
-                   "AND NOT EXISTS (SELECT 1 FROM board b WHERE b.recipe_seq = r.recipe_seq) " +
-                   "ORDER BY r.recipe_seq DESC", nativeQuery = true)
-    List<Object[]> findRecipesByUser(@Param("userSeq") Long userSeq);
 }
