2ba6975a8b5f6d0cd886ee05851cbc0a0aeb43ff
Heo Jaewon <112384684+Heo-Jae-Won@users.noreply.github.com>
2025-03-16 11:02:02 +0900

    Feature/board (#80)
    * detail 페이지 수정, 삭제버튼 추가
    
    * 게시글 업데이트 미완완
    
    * 비로그인시 댓글 보이게 수정정
    
    * 게시물 삭제기능 미완성성
    
    * 게시물 수정/삭제기능 완성
    
    * 글쓰기에 레시피 선택기능 추가 및 boardUpdate, boardWrite 각각 css,js파일 분리
    
    * 작성자의 게시물 수정/삭제버튼 로딩 오류 해결
    
    * 레시피 추천받기 사진 눌렀을때 /recipe/recommend로 연결
    
    * 메소드 네이밍 변경경
    
    * 저장된 레시피를 이용해 게시물 작성시 boardWrite의 셀렉옵션에 그 레시피는 더 이상 나타나지 않게 수정
    
    * 게시판 작성 시 recipe 목록 가져오는 로직 변경
    
        nativeQuery -> DTO projection JPQL로 변경
            -- Object type 보단 명확한 DTO로
            -- nativeQuery보다는 JPA의 JPQL로
            -- JPQL 전환에 따른 entity 관계 변경
    	-- 새로운 method 추가에 따른 이전사용 method 제거
    
    * boardDetail에서 recipe output 가져오는 로직 변경
        -- CreateEntityManager --> JPQL
        -- recipe 관련 로직 recipe domain으로 이동
        -- 해당 service naming에 맞춰 get --> select 변경
    
    * method camel case에 맞춤 및 @Transactional 추가가
    
    * import organize
    
    * tab size 2 -> 4로 변경
    
    * 불필요한 load event 감싸기 제거
    
    * 안쓰게 된 natvie query 삭제
    
    ---------
    
    Co-authored-by: KIM JAEHYOUNG <becomeclear8757@gmail.com>

--- src/main/java/org/scit/project/recipe/repository/RecipeOutputRepository.java
+++ src/main/java/org/scit/project/recipe/repository/RecipeOutputRepository.java
@@ -1,9 +1,12 @@
 package org.scit.project.recipe.repository;
 
+import java.util.Optional;
+
+import org.scit.project.recipe.entity.RecipeEntity;
 import org.scit.project.recipe.entity.RecipeOutputEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RecipeOutputRepository extends JpaRepository<RecipeOutputEntity, Long> {
 
-    
+    Optional<RecipeOutputEntity> findByRecipeEntity(RecipeEntity recipeEntity);
 } 
