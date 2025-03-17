8d385a0e2e0c36d3cdea0dc82d4c5bee8cfb9223
Heo-Jae-Won <ckdwhgood2@naver.com>
2025-03-17 23:23:46 +0900

    UX 개선에 따른 loading 방식 변경
    error:
    mainRepository query들에서 오류 발생
    findAllWithHeartCountAndIsDeletedIsFalseContainingBoardTitle 오류
    findTopPostsByPeriodAndHeartCountAndIsDeletedIsFalse 오류
    
    기존의 board : recipe 는 1 : 1 관계였음. eager로 조회할 board가 없음.
    그런데 UX 개선에 따라 board : recipe = n : 1 관계로 변경됨.
    그 경우 위의 query method가 복수 개의 board를 찾아 fetch함
    즉, duplicate row 오류 발생
    
    cause:
    @ManyToOne의 default가 Eager라서 전부 fetch하는 것이 문제
    
    solution:
    Lazy로 명시하여 필요할 때만 로딩하게 변경해 duplicate row 해결
    위의 query는 recipeEntity가 애초에 불필요한 query들임

--- src/main/java/org/scit/project/board/entity/BoardEntity.java
+++ src/main/java/org/scit/project/board/entity/BoardEntity.java
@@ -11,6 +11,7 @@ import org.scit.project.user.entity.UserEntity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -40,7 +41,7 @@ public class BoardEntity {
     @Column(name = "board_seq")
     private Long boardSeq;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_seq", referencedColumnName = "user_seq")
     private UserEntity userEntity;
 
@@ -64,7 +65,7 @@ public class BoardEntity {
     @Column(name = "is_deleted")
     private Boolean isDeleted;
     
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "recipe_seq",referencedColumnName = "recipe_seq")
     private RecipeEntity recipeEntity;
 
